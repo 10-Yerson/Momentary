f8f31a5e8aa5cfd0f12a32f7e35549d4154ab57a
10-Yerson <Dreyerg9@gmail.com>
2024-10-12 18:01:38 -0500

    Perfiles de usuarios 📲

--- src/app/client/userprofile/[id]/components/ProfileHeader.jsx
+++ src/app/client/userprofile/[id]/components/ProfileHeader.jsx
@@ -6,6 +6,8 @@ import { useParams } from 'next/navigation';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FaCog } from 'react-icons/fa';
+import UserSeguidores from './UserSeguidores';
+import UserSeguidos from './UserSeguidos';
 
 export default function ProfileHeader() {
   const { id } = useParams();
@@ -13,6 +15,8 @@ export default function ProfileHeader() {
   const [loading, setLoading] = useState(true);
   const [isFollowing, setIsFollowing] = useState(false);
   const [isFollowedBy, setIsFollowedBy] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [OpenIs, setOpen] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -57,12 +61,12 @@ export default function ProfileHeader() {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-      <div class="flex flex-row gap-2">
-        <div class="w-7 h-7 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
-        <div class="w-7 h-7 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
-        <div class="w-7 h-7 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+        <div class="flex flex-row gap-2">
+          <div class="w-7 h-7 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+          <div class="w-7 h-7 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
+          <div class="w-7 h-7 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+        </div>
       </div>
-    </div>
     );
   }
 
@@ -73,21 +77,31 @@ export default function ProfileHeader() {
       ? 'Seguir de vuelta'
       : 'Seguir';
 
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const Modaltoggle = () => {
+    setOpen(!OpenIs);
+  };
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white p-4 md:p-6 lg:p-8">
       <div className="flex flex-col md:flex-row items-center text-center md:text-left">
         <img
           alt="Profile picture of user"
           className="rounded-full border border-gray-200 shadow-lg w-24 h-24 md:w-32 md:h-32 object-cover"
-          src={user.profilePicture || "https://storage.googleapis.com/a1aa/image/Mu0ExRQkd0JqAtVC0PkQNVV8VJfpubi9jIe6TyXjgQBngzjTA.jpg"}
+          src={user.profilePicture || "https://res.cloudinary.com/dbgj8dqup/image/upload/v1725640005/uploads/ktsngfmjvjv094hygwsu.png"}
         />
         <div className="mt-4 md:mt-0 md:ml-6">
           <h2 className="text-xl md:text-2xl font-bold text-gray-800">{user.name} {user.apellido}</h2>
           <div className="flex justify-center md:justify-start space-x-4 mt-2 text-gray-600">
             <div><span className="font-bold">{user.postsCount || 0}</span> publicaciones</div>
-            <div><span className="font-bold">{user.followers.length || 0}</span> seguidores</div>
-            <div><span className="font-bold">{user.following.length || 0}</span> seguidos</div>
+            <div onClick={toggleModal}><span  className="font-bold">{user.followers.length || 0}</span> seguidores</div>
+            <div onClick={Modaltoggle}><span className="font-bold">{user.following.length || 0}</span> seguidos</div>
           </div>
+          <UserSeguidores isOpen={isOpen} toggleModal={toggleModal} />
+          <UserSeguidos OpenIS={OpenIs} Modaltoggle={Modaltoggle} />
           <div className="mt-4 flex justify-center md:justify-start space-x-2">
             <button
               className={`px-4 py-1 rounded ${isFollowing ? 'bg-gray-200 text-gray-800' : 'bg-blue-500 text-white'}`}
---
+++ src/app/client/userprofile/[id]/components/UserSeguidores.jsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import axios from '../../../../../utils/axios';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+
+export default function UserSeguidores({ isOpen, toggleModal }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const fetchFollowersData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(`/api/followers/seguidores/${id}`);
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFollowersData();
+  }, [isOpen, id]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white p-6 rounded shadow-lg lg:w-[50%] md:w-[50%] w-[90%] relative">
+        <button
+          onClick={toggleModal}
+          className="absolute top-3 right-3 text-gray-500 hover:text-gray-700 text-3xl"
+        >
+          &times;
+        </button>
+        <h2 className="text-2xl font-bold mb-4 sm:text-lg md:text-xl">Seguidores</h2>
+
+        {loading ? (
+          <div className="flex justify-center items-center w-1/2">
+            <div class="relative flex w-64 animate-pulse gap-2 p-4">
+              <div class="h-12 w-12 rounded-full bg-slate-400"></div>
+              <div class="flex-1">
+                <div class="mb-1 h-5 w-3/5 rounded-lg bg-slate-400 text-lg"></div>
+                <div class="h-5 w-[90%] rounded-lg bg-slate-400 text-sm"></div>
+              </div>
+              <div class="absolute bottom-5 right-0 h-4 w-4 rounded-full bg-slate-400"></div>
+            </div>
+          </div>
+        ) : data.length === 0 ? (
+          <p className="text-center text-gray-500">Este usuario aún no tiene seguidores.</p>
+        ) : (
+          <div className="mt-4 space-y-4 max-h-[400px] overflow-y-auto">
+            {data.map((follower) => (
+              <div key={follower._id} className="flex items-center space-x-4 border-b pb-4">
+                <Link href={`/client/userprofile/${follower._id}`}>
+                  <img
+                    src={follower.profilePicture}
+                    alt={`${follower.name} ${follower.apellido}`}
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                </Link>
+                <div>
+                  <Link href={`/client/userprofile/${follower._id}`}>
+                    <p className="font-semibold text-gray-800">{follower.name} {follower.apellido}</p>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
