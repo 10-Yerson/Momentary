11db1a25201f8b2739ce2d63d2ac527f0d4ca93e
10-Yerson <Dreyerg9@gmail.com>
2024-10-14 13:53:42 -0500

    Error

--- src/app/client/components/Notificaciones/notificaciones.jsx
+++ src/app/client/components/Notificaciones/notificaciones.jsx
@@ -1,43 +1,34 @@
-'use client'
+'use client';
 
 import React, { useEffect, useState } from 'react';
 import axios from '../../../../utils/axios';
-import { io } from 'socket.io-client';
 
 export default function Notification() {
   const [notifications, setNotifications] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Obtener notificaciones cuando se monta el componente
     const fetchNotifications = async () => {
       try {
         const response = await axios.get('/api/notifications');
         setNotifications(response.data);
       } catch (error) {
         console.error('Error al obtener notificaciones:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
+
     fetchNotifications();
-
-    // Conectar a Socket.io para notificaciones en tiempo real
-    const socket = io(process.env.NEXT_PUBLIC_BASE_URL);
-    // Cambia esto a la URL del servidor si está en producción
-
-    socket.on('newNotification', (newNotification) => {
-      setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-    });
-
-    // Limpiar la conexión cuando el componente se desmonta
-    return () => {
-      socket.disconnect();
-    };
   }, []);
 
   return (
     <div className="w-full md:w-1/2 p-4">
       <h2 className="text-lg font-bold mb-4">Notificaciones</h2>
       <div className="space-y-4">
-        {notifications.length === 0 ? (
+        {isLoading ? (
+          <p>Cargando notificaciones...</p>
+        ) : notifications.length === 0 ? (
           <p>No tienes notificaciones.</p>
         ) : (
           notifications.map((notification) => (
