789ba3768a7f73961176ea6e77535ddfaf6a8dab
10-Yerson <Dreyerg9@gmail.com>
2024-12-24 10:26:22 -0500

    Arreglo Deploy

---
+++ src/app/client/adiction/page.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { MyProvider, useState } from '../context/MyContext';
+import UserPanel from '../components/Dasboard/siderbar'
+import Adiction from '../components/Perfil/adiction';
+
+export default function ViewPublication() {
+
+    return (
+        <MyProvider>
+            <UserPanel />
+            <div className='flex overflow-hidden select-none md:ml-40 ml-0 flex-1 md:flex-row'>
+                <Adiction/>
+            </div>
+        </MyProvider>
+    );
+}
