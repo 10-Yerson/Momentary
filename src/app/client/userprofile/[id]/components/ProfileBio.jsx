157834e7981f70f1709dd099d5798c5002963c37
10-Yerson <Dreyerg9@gmail.com>
2025-02-10 18:31:20 -0500

    User Profile Csm

--- src/app/client/userprofile/[id]/components/ProfileBio.jsx
+++ src/app/client/userprofile/[id]/components/ProfileBio.jsx
@@ -1,24 +1,55 @@
-const ProfileBio = () => (
-  <div className="mt-5 text-center md:text-left">
-    <p className="text-sm md:text-base lg:text-lg text-gray-800 leading-relaxed">
-      Desarrollador(a) de Software
-      <br />
-      Apasionado(a) por la tecnología y el código <span className="text-purple-500">💻</span>!
-      <br />
-      Universidad Nacional de Colombia
-    </p>
-    <a 
-      className="text-blue-500 hover:text-blue-600 transition-colors mt-2 block"
-      href="#"
-      target="_blank" 
-      rel="noopener noreferrer"
-    >
-      Perfil de LinkedIn
-    </a>
-    <p className="mt-2 text-gray-500 text-xs md:text-sm lg:text-base">
-      Carlos sigue esta cuenta
-    </p>
-  </div>
-);
+'use client';
 
-export default ProfileBio;
+import { useState, useEffect } from 'react';
+import axios from '../../../../../utils/axios';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import { BsPersonLinesFill, BsHouseDoorFill, BsHeartFill } from "react-icons/bs";
+
+export default function ProfileBio() {
+
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/user/${id}`);
+        setData(response.data);
+        console.log('Respuesta Data dd ', response.data)
+      } catch (error) {
+        console.error('Error al obtener notificaciones:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="mt-4 w-4/5 md:w-3/5 lg:w-1/2 md:mx-auto lg:mx-0 mx-auto rounded-md">
+      <h2 className="text-lg font-semibold mb-4">Detalles</h2>
+      {data?.profile?.descripcion && (
+        <div className="flex items-center mb-2">
+          <BsPersonLinesFill className="text-gray-600 text-2xl mr-2 flex-shrink-0" />
+          <span className="break-words">{data.profile.descripcion}</span>
+        </div>
+      )}
+      {data?.profile?.origen && (
+        <div className="flex items-center mb-4">
+          <BsHouseDoorFill className="text-gray-600 text-2xl mr-2" />
+          <span>
+            De <span className="font-semibold">{data.profile.origen}</span>
+          </span>
+        </div>
+      )}
+      {data?.profile?.estadoCivil && (
+        <div className="flex items-center mb-4">
+          <BsHeartFill className="text-gray-600 text-2xl mr-2" />
+          <span>{data.profile.estadoCivil}</span>
+        </div>
+      )}
+    </div>
+
+  )
+}
+
