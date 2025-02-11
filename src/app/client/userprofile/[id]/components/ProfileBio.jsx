3b81a1ab09f8ca46ac9a753c9c8a4d809c018480
10-Yerson <Dreyerg9@gmail.com>
2025-02-11 10:07:56 -0500

    Msg not Info User

--- src/app/client/userprofile/[id]/components/ProfileBio.jsx
+++ src/app/client/userprofile/[id]/components/ProfileBio.jsx
@@ -28,27 +28,49 @@ export default function ProfileBio() {
   return (
     <div className="mt-4 w-4/5 md:w-3/5 lg:w-1/2 md:mx-auto lg:mx-0 mx-auto rounded-md">
       <h2 className="text-lg font-semibold mb-4">Detalles</h2>
-      {data?.profile?.descripcion && (
-        <div className="flex items-center mb-2">
-          <BsPersonLinesFill className="text-gray-600 text-2xl mr-2 flex-shrink-0" />
-          <span className="break-words">{data.profile.descripcion}</span>
+
+      {data?.profile?.descripcion || data?.profile?.origen || data?.profile?.estadoCivil ? (
+        <>
+          {data?.profile?.descripcion && (
+            <div className="flex items-center mb-2">
+              <BsPersonLinesFill className="text-gray-600 text-2xl mr-2 flex-shrink-0" />
+              <span className="break-words">{data.profile.descripcion}</span>
+            </div>
+          )}
+          {data?.profile?.origen && (
+            <div className="flex items-center mb-4">
+              <BsHouseDoorFill className="text-gray-600 text-2xl mr-2" />
+              <span>
+                De <span className="font-semibold">{data.profile.origen}</span>
+              </span>
+            </div>
+          )}
+          {data?.profile?.estadoCivil && (
+            <div className="flex items-center mb-4">
+              <BsHeartFill className="text-gray-600 text-2xl mr-2" />
+              <span>{data.profile.estadoCivil}</span>
+            </div>
+          )}
+        </>
+      ) : (
+        <div class="flex items-center p-3 w-full bg-white rounded-md">
+          <section class="flex justify-center items-center w-14 h-14 rounded-full shadow-md overflow-hidden">
+            <img src={data.profilePicture} alt="Foto de perfil" class="w-full h-full object-cover rounded-full"/>
+          </section>
+
+          <section class="block border-l border-gray-300 m-3">
+            <div class="pl-3">
+              <h3 class="text-gray-600 font-semibold text-sm">{data.name} {data.apellido}</h3>
+              <h3 class="text-gray-700 text-lg font-bold">
+                No hay información disponible
+              </h3>
+            </div>
+          </section>
         </div>
-      )}
-      {data?.profile?.origen && (
-        <div className="flex items-center mb-4">
-          <BsHouseDoorFill className="text-gray-600 text-2xl mr-2" />
-          <span>
-            De <span className="font-semibold">{data.profile.origen}</span>
-          </span>
-        </div>
-      )}
-      {data?.profile?.estadoCivil && (
-        <div className="flex items-center mb-4">
-          <BsHeartFill className="text-gray-600 text-2xl mr-2" />
-          <span>{data.profile.estadoCivil}</span>
-        </div>
+
       )}
     </div>
+
 
   )
 }
