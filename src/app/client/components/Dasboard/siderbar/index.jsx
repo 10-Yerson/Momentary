7705ed71a46d1e241891f16f172a0d4553335611
10-Yerson <Dreyerg9@gmail.com>
2025-03-02 13:11:01 -0500

    agregar iconos de navegación con relleno

--- src/app/client/components/Dasboard/siderbar/index.jsx
+++ src/app/client/components/Dasboard/siderbar/index.jsx
@@ -3,8 +3,12 @@
 import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
-import { FiHome, FiMessageSquare, FiSearch, FiVideo, FiBell, FiUser, FiMoreHorizontal } from "react-icons/fi";
-
+import { HiHome } from "react-icons/hi";
+import { RiMessage3Fill, RiSearchFill } from "react-icons/ri";
+import { MdVideoLibrary } from "react-icons/md"; 
+import { IoNotifications } from "react-icons/io5";
+import { FaUserCircle } from "react-icons/fa";
+import { BsThreeDots } from "react-icons/bs";
 
 export default function UserPanel() {
 
@@ -16,8 +20,7 @@ export default function UserPanel() {
       <li className="mt-3 p-2 rounded-lg">
         <a
           href={href}
-          className={`flex flex-col items-center pb-1 transition-all duration-300 ${isActive ? "border-b-2 border-blue-500 text-blue-600 dark:text-blue-300" : "hover:text-blue-600 dark:hover:text-blue-300"
-            }`}
+          className={`flex flex-col items-center pb-1 transition-all duration-300 ${isActive ? "border-b-2 border-black" : "hover:text-blue-600 dark:hover:text-blue-300"}`}
         >
           <Icon className="h-5 w-5" />
           <span className="text-xs mt-2 tracking-wide">{label}</span>
@@ -43,16 +46,16 @@ export default function UserPanel() {
           </div>
 
           <ul className="flex md:flex-col md:mt-2 text-black capitalize space-x-4 md:space-x-0">
-            <NavItem href="/client" Icon={FiHome} label="Home" />
-            <NavItem href="/client/messages" Icon={FiMessageSquare} label="Mensajes" />
-            <NavItem href="/client/search" Icon={FiSearch} label="Buscar" />
-            <NavItem href="/client/videos" Icon={FiVideo} label="Videos" />
-            <NavItem href="/client/notifications" Icon={FiBell} label="Notificaciones" />
-            <NavItem href="/client/profile" Icon={FiUser} label="Perfil" />
+            <NavItem href="/client" Icon={HiHome} label="Home" />
+            <NavItem href="/client/messages" Icon={RiMessage3Fill} label="Mensajes" />
+            <NavItem href="/client/search" Icon={RiSearchFill} label="Buscar" />
+            <NavItem href="/client/videos" Icon={MdVideoLibrary} label="Videos" />
+            <NavItem href="/client/notifications" Icon={IoNotifications} label="Notificaciones" />
+            <NavItem href="/client/profile" Icon={FaUserCircle} label="Perfil" />
           </ul>
 
           <div className="mt-auto flex items-center space-x-3 p-3 dark:text-blue-500 rounded-full md:flex">
-            <FiMoreHorizontal className="h-7 w-7" />
+            <BsThreeDots className="h-7 w-7" />
           </div>
         </nav>
 
